130c013e447fd543ffc25ab979315bfa0099e8c5
yuchen hu <yuchebhu@outlook.com>
2019-07-06 21:39:45 +0800

    测试稀疏图和稠密图的迭代器的使用

--- Algorithms/Graph Base/main.cpp
+++ Algorithms/Graph Base/main.cpp
@@ -1,5 +1,48 @@
 #include <iostream>
+#include <ctime>
+#include "SparseGraph.h"
+#include "DenseGraph.h"
 
 int main() {
+	int N = 20;
+	int M = 100;
+
+	srand(time(NULL));
+
+	SparseGraph graph1(N, false);
+
+	for (int i = 0; i < M; ++i) {
+		int a = rand() % N;
+		int b = rand() % N;
+		graph1.addEdge(a, b);
+	}
+
+	for (int v = 0; v < N; ++v) {
+		std::cout << v << " : ";
+		SparseGraph::adjIterator adj(graph1, v);
+		for (int w = adj.begin(); !adj.end(); w = adj.next())
+			std::cout << w << " ";
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	
+
+	DenseGraph graph2(N, false);
+
+	for (int i = 0; i < M; ++i) {
+		int a = rand() % N;
+		int b = rand() % N;
+		graph2.addEdge(a, b);
+	}
+
+	for (int v = 0; v < N; ++v) {
+		std::cout << v << " : ";
+		DenseGraph::adjIterator adj(graph2, v);
+		for (int w = adj.begin(); !adj.end(); w = adj.next())
+			std::cout << w << " ";
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
 	return 0;
 }
