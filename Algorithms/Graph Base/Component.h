ec7ac550c9b6c13556056ec54ac910fdf1a87dda
yuchen hu <yuchebhu@outlook.com>
2019-07-07 21:51:22 +0800

    添加计算图中连通分量个数的类

--- Algorithms/Graph Base/Component.h
+++ Algorithms/Graph Base/Component.h
@@ -10,11 +10,12 @@ class Component {
 private:
 	Graph& G;
 	bool* visited;
+	int* id; // 使用并查集的思想，判断两个节点是否连通；
 	int ccount;
 
 	void dfs(int v) {
 		visited[v] = true;
-
+		id[v] = ccount;
 		typename Graph::adjIterator adj(G, v);
 		for (int i = adj.begin(); !adj.end(); i = adj.next()) {
 			if (!visited[i])
@@ -24,10 +25,13 @@ private:
 public:
 	Component(Graph& graph) : G(graph) {
 		visited = new bool[G.V()];
+		id = new int[G.V()];
 
 		ccount = 0;
-		for (int i = 0; i < G.V(); ++i)
+		for (int i = 0; i < G.V(); ++i) {
 			visited[i] = false;
+			id[i] = -1;
+		}
 
 		for (int i = 0; i < G.V(); ++i)
 			if (!visited[i]) {
@@ -42,5 +46,12 @@ public:
 
 	int count() {
 		return ccount;
+	}
+
+	bool isConnected(int v, int w) {
+		assert(v >= 0 && v < G.V());
+		assert(w >= 0 && w < G.V());
+
+		return id[v] == id[w];
 	}
 };
