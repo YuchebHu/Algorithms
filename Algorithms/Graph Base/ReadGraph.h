0dde1efc18281f4b2fc356ea177161adb8902510
yuchen hu <yuchebhu@outlook.com>
2019-07-07 21:21:00 +0800

    增加从文件中读取图的模板方法

---
+++ Algorithms/Graph Base/ReadGraph.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+
+template <typename Graph>
+class ReadGraph {
+public:
+	ReadGraph(Graph& graph, const std::string& filename) {
+		std::ifstream file(filename);
+		std::string line;
+		int V;
+		int E;
+		
+		assert(file.is_open());
+		assert(getline(file, line));
+
+		std::stringstream ss(line);
+		ss >> V >> E;
+
+		assert(V == graph.V());
+
+		for (int i = 0; i < E; ++i) {
+			assert(getline(file, line));
+
+			std::stringstream ss(line);
+			int a;
+			int b;
+			ss >> a >> b;
+
+			assert(a >= 0 && a < V);
+			assert(b >= 0 && b < V);
+			graph.addEdge(a, b);
+		}
+	}
+};
