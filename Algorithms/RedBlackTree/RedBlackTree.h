cc5e6f18a60c9f97ca941c986edfd546b758ce09
yuchen hu <yuchebhu@outlook.com>
2019-07-24 17:54:25 +0800

    添加红黑树的插入

--- Algorithms/RedBlackTree/RedBlackTree.h
+++ Algorithms/RedBlackTree/RedBlackTree.h
@@ -24,7 +24,7 @@ private:
 			this->left = this->right = NULL;
 		}
 
-		Node(Node node) {
+		Node(Node* node) {
 			this->key = node.key;
 			this->value = node.key;
 			this->color = node.color;
@@ -44,13 +44,28 @@ private:
 		return node->color == RED;
 	}
 
+	void flipColors(Node* node) {
+		node->color = RED;
+		node->left->color = BLACK;
+		node->right->color = BLACK;
+	}
+
 public:
 	RedBlackTree() : root(NULL), count(0), height(0) {
-
+		std::cout << "create!\n";
 	}
 
 	~RedBlackTree() {
 		destory();
+	}
+
+	void insert(Key key, Value value) {
+		root = __insert(root, key, value);
+		root->color = BLACK;
+	}
+
+	bool contain(Key key) {
+		__contain(root, key);
 	}
 
 	void preOrder() {
@@ -73,22 +88,8 @@ public:
 		return count;
 	}
 
-	Node* rotateLeft(Node* node) {
-		Node* x(node->right);
-		node->right = x->left;
-		x->left = node;
-		x->color = node->color;
-		node->color = RED;
-		return x;
-	}
-
-	Node* rotateRight(Node* node) {
-		Node* x(node->left);
-		node->left = x->right;
-		x->right = node;
-		x->color = node->color;
-		node->color = RED;
-		return x;
+	int Height() {
+		return __Height(root);
 	}
 private:
 	int __size(Node* node) {
@@ -102,7 +103,7 @@ private:
 	}
 
 	void __preOrder(Node* node) {
-		if (node != null) {
+		if (node != NULL) {
 			std::cout << node->value << std::endl;
 			__preOrder(node->left);
 			__preOrder(node->right);
@@ -112,8 +113,8 @@ private:
 	void __inOrder(Node* node) {
 		if (node != NULL) {
 			__inOrder(node->left);
-			std::cout << node->value << std::endl;
 			__inOrder(node->right);
+			std::cout << node->value << std::endl;
 		}
 	}
 
@@ -134,5 +135,73 @@ private:
 		}
 	}
 
+	Node* rotateLeft(Node* node) {
+		Node* x(node->right);
+		node->right = x->left;
+		x->left = node;
+		x->color = node->color;
+		node->color = RED;
 
+		return x;
+	}
+
+	Node* rotateRight(Node* node) {
+		Node* x(node->left);
+		node->left = x->right;
+		x->right = node;
+		x->color = node->color;
+		node->color = RED;
+
+		return x;
+	}
+
+	Node* __insert(Node* node, Key key, Value value) {
+		if (node == NULL) {
+			++count;
+			return new Node(key, value, RED);
+		}
+
+		if (node->key > key)
+			node->left = __insert(node->left, key, value);
+		else if (node->key < key)
+			node->right = __insert(node->right, key, value);
+		else // node->key == key
+			node->value = value;
+
+		if (isRed(node->right) && !isRed(node->left)) // 右节点是红色
+			node = rotateLeft(node);
+		if (isRed(node->left) && isRed(node->left->left)) // 两个连续的左节点是红色
+			node = rotateRight(node);
+		if (isRed(node->left) && isRed(node->right)) // 左右两个节点都是红色
+			flipColors(node);
+		//std::cout << key << std::endl;
+		return node;
+	}
+
+	bool __contain(Node* node, Key key) {
+		if (node == NULL)
+			return false;
+		if (node->key == key)
+			return true;
+		else if (node->key > key)
+			return __contain(node->left, key);
+		else
+			return __contain(node->right, key);
+	}
+
+	int __Height(Node* node) {
+		int heightCount = 0;
+		int heightLeft = 0;
+		int heightRight = 0;
+		if (node == NULL) {
+			heightCount = 0;
+			return heightCount;
+		}
+
+		heightLeft = __Height(node->left);
+		heightRight = __Height(node->right);
+		heightCount = 1 + ((heightLeft > heightRight) ? heightLeft : heightRight);
+		return heightCount;
+	}
 };
+
